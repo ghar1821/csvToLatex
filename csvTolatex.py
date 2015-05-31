656dc54be057b9c9606fb46f71b56b75bbbdcad4
ghar1821 <ghar1821@gmail.com>
2015-05-31 19:22:00 +1000

    Added script

---
+++ csvTolatex.py
@@ -0,0 +1,24 @@
+def csvToLatex(csvFile):
+	csv = open(csvFile,'r')
+	line = csv.readline().replace('_','\\_').split(',')
+	latex = '\\begin{table}[h]\n\\begin{tabular}{'
+	for x in xrange(0,len(line)):
+		latex += '|l'
+	latex += '|}\n' + '\hline\n'
+	for x in xrange(0,len(line)):
+		if x == len(line) - 1:
+			latex += line[x].strip() + ' \\\\' + ' \hline\n'
+		else:
+			latex += line[x].strip() + ' & '
+	for line in csv:
+		row = line.replace('_','\\_').split(',')
+		for x in xrange(0,len(row)):
+			if x == len(row) - 1:
+				latex += row[x].strip() + ' \\\\' + ' \hline\n'
+			else:
+				latex += row[x].strip() + ' & '
+	latex += '\end{tabular}\n\end{table}'
+	print latex
+	csv.close()
+
+csvToLatex('/Users/MacbookPro/Dropbox/sample.csv')
